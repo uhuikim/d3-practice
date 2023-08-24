f1fafcf3fd872791550396e33fa8d253488449d8
uhuikim <uhui94@naver.com>
2023-08-24 19:40:58 +0900

    feat: 평균선 그리기

--- histogram/draw-histogram.js
+++ histogram/draw-histogram.js
@@ -77,6 +77,26 @@ async function drawScatter() {
     .attr("fill", "darkgrey")
     .style("font-size", "12px")
     .style("font-family", "sans-serif");
+
+  const mean = d3.mean(dataset, metricAccessor);
+
+  const meanLine = bounds
+    .append("line")
+    .attr("x1", xScale(mean))
+    .attr("x2", xScale(mean))
+    .attr("y1", -15)
+    .attr("y2", dimensions.boundedHeight)
+    .attr("stroke", "maroon")
+    .attr("stroke-dasharray", "2px 4px");
+
+  const meanLabel = bounds
+    .append("text")
+    .attr("x", xScale(mean))
+    .attr("y", -20)
+    .text("mean")
+    .attr("fill", "maroon")
+    .style("font-size", "12px")
+    .style("text-anchor", "middle");
 }
 
 drawScatter();
